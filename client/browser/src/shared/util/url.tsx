fa985a92f3174dadbfeb9ff60c5a6c2ecf86e8ec
Quinn Slack <qslack@qslack.com>
2018-12-14 02:16:14 -0800

    dedupe toAbsoluteBlobURL

--- client/browser/src/shared/util/url.tsx
+++ client/browser/src/shared/util/url.tsx
@@ -1,17 +1,7 @@
-import { Position } from '@sourcegraph/extension-api-types'
-import { AbsoluteRepoFile, PositionSpec } from '../../../../../shared/src/util/url'
+import { AbsoluteRepoFile, PositionSpec, toPrettyBlobURL } from '../../../../../shared/src/util/url'
 import { repoUrlCache, sourcegraphUrl } from './context'
 
-function toPositionHash(position?: Position): string {
-    if (!position) {
-        return ''
-    }
-    return '#L' + position.line + (position.character ? ':' + position.character : '')
-}
-
 export function toAbsoluteBlobURL(ctx: AbsoluteRepoFile & Partial<PositionSpec>): string {
-    const rev = ctx.commitID ? ctx.commitID : ctx.rev
     const url = repoUrlCache[ctx.repoName] || sourcegraphUrl
-
-    return `${url}/${ctx.repoName}${rev ? '@' + rev : ''}/-/blob/${ctx.filePath}${toPositionHash(ctx.position)}`
+    return `${url}/${toPrettyBlobURL(ctx)}`
 }
